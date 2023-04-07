29c7a61223e35186d9ab05a0ec1c31880c6bf23e
andiharun29 <andiharunalrasyid14@gmail.com>
2023-04-07 10:31:41 +0700

    masukkan maksimal datanya

---
+++ exercise/exercise.cpp
@@ -0,0 +1,5 @@
+//1.suatu algoritma sangat diperlukan untuk menyelesaikan masalah karena memiliki langkah-langkah yang mempunyai struktur yang bisa memudahkan kita untuk menyelesaikan suatu masalah.
+//2.struktur data linear dan non-linear
+//3.banyaknya langkah, besar dan jenis data yang di proses, penggunaan sistem
+//4.menurut saya yang paling mudah itu bublesort kenapa karena dia hanya membandingkan antara 2 bilangan jadi kita hanya menentukan dia lebih besar dari bilangan sebelumnya atau lebih kecil
+//5.yang termasuk quadratic : bubblesort,insertionsort dan selectionsort. sedangkan yang termasuk loglinear adalah : mergesort, quicksort
---
+++ exercise2.1/exercise2.1.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+using namespace std;
+
+const int MAX_DATA = 119; // 99 + 20 -2 x 15 + 10 + 20 ( nim 99)
+
