16fd54e711f0fd8b873057766c12eb66bb81e1ce
andiharun29 <andiharunalrasyid14@gmail.com>
2023-04-07 10:35:24 +0700

    memasukkan syarat pertama i<=mid&&AR <= high

--- exercise2.1/exercise2.1.cpp
+++ exercise2.1/exercise2.1.cpp
@@ -6,4 +6,15 @@ const int MAX_DATA = 119; // 99 + 20 -2 x 15 + 10 + 20 ( nim 99)
 void merge(int harun[], int low, int mid, int high) {       //arr diganti dengan nama panggilan 
     int i = low, AR = mid + 1, k = low;                     //j diganti dengan inisial nama awal dan terahir nama saya andi harun al rasyid
     int b[MAX_DATA];
+    while (i <= mid && AR <= high) {
+        if (harun[i] <= harun[AR]) {
+            b[k] = harun[i];
+            i++;
+        }
+        else {
+            b[k] = harun[AR];
+            AR++;
+        }
+        k++;
+    }
     
