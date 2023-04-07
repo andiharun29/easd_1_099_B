1a0e4a2ac147be70e16018cd7303cc09436c12ec
andiharun29 <andiharunalrasyid14@gmail.com>
2023-04-07 10:33:51 +0700

    menambahkan perintah void dan syarat2

--- exercise2.1/exercise2.1.cpp
+++ exercise2.1/exercise2.1.cpp
@@ -3,3 +3,7 @@ using namespace std;
 
 const int MAX_DATA = 119; // 99 + 20 -2 x 15 + 10 + 20 ( nim 99)
 
+void merge(int harun[], int low, int mid, int high) {       //arr diganti dengan nama panggilan 
+    int i = low, AR = mid + 1, k = low;                     //j diganti dengan inisial nama awal dan terahir nama saya andi harun al rasyid
+    int b[MAX_DATA];
+    
